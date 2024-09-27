aa6b8b11393d450a9a6ec8baaea34977a6e713a7
Amit Singh <amitsinghbxr@gmail.com>
2024-09-27 19:44:06 +0530

    date format with cellrendrer function

--- src/app/offertable/offertable.component.ts
+++ src/app/offertable/offertable.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
-import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
+import { ColDef, ValueGetterParams } from 'ag-grid-community'; // Column Definition Type Interface
 import { DataService } from '../data.service';
-
+import moment_ from 'moment';
+const moment = moment_;
 @Component({
   selector: 'app-offertable',
   standalone: true,
@@ -14,12 +15,24 @@ export class OffertableComponent implements OnInit {
   rowData: any;
   constructor(private dataservice: DataService) {}
   columnDefs: ColDef[] = [
-    { headerName: 'Id', field: 'id' },
-    { headerName: 'First Name', field: 'first_name' },
-    { headerName: 'Last Name', field: 'last_name' },
+    {
+      headerName: 'Id',
+      field: 'id',
+      cellRenderer: (p: any) => +p.data.id + 1000,
+    },
+    {
+      headerName: 'Full Name',
+      cellRenderer: (p: any) => p.data.first_name + ' ' + p.data.last_name,
+    },
     { headerName: 'Email', field: 'email' },
     { headerName: 'Gender', field: 'gender' },
-    { headerName: 'DOB', field: 'Birthday' },
+    {
+      headerName: 'DOB',
+      field: 'Birthday',
+      cellRenderer: (data: any) => {
+        return moment(data.value).format('DD MMM YYYY');
+      },
+    },
   ];
   ngOnInit(): void {
     this.dataservice.getData().subscribe((data) => {
